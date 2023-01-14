bd73a22faababf911ea923b0f2c64a9a03da8ed7
Winshipe <winshipea@gmail.com>
2023-01-14 17:51:21 +0100

    Update tokenize_sequence.py

--- scripts/tokenize_sequence.py
+++ scripts/tokenize_sequence.py
@@ -5,13 +5,13 @@ import os
 
 max_sz = 10**7
 
-infile_good = snakemake.input[0]#"good_overlaps.txt" #sys.argv[1]
-infile_bad = snakemake.input[1]#"bad_overlaps.txt"
-vocab_file = snakemake.input[2]#"5mer.vocab.txt"
+infile = snakemake.input[0]#"good_overlaps.txt" #sys.argv[1]
+vocab_file = snakemake.input[1]#"bad_overlaps.txt"
+#vocab_file = snakemake.input[2]#"5mer.vocab.txt"
 k = int(snakemake.params[0])
 outfile = os.path.dirname(snakemake.output[0]) #"tokenized_refined_good.tf" #sys.argv[2]
-line_size = 150
-n_ctx = 2 # the number of nts either side to consider, eg (AA[G]AA)
+#line_size = 150
+n_ctx = int((k-1)//2) # the number of nts either side to consider, eg (AA[G]AA)
 #kmers = np.zeros((max_sz, line_size),dtype='int32')
 #labels = np.zeros(max_sz, dtype='int32')
 """
@@ -19,8 +19,8 @@ We need to interleave the 2 datasets s.t. the labels are 01010101 and so on othe
 the training, val and test sets will be unbalanced and we'll just teach it to call 1 class
 """
 
-kmers_g = np.zeros((max_sz,line_size),dtype='int32')
-labels_g = np.zeros(max_sz, dtype='int32')
+kmers = np.zeros((max_sz,17),dtype='int32')
+labels = np.zeros(max_sz, dtype='int32')
 
 tokens = dict()
 c = 2
@@ -29,6 +29,7 @@ with open(vocab_file) as f:
     for line in f:
         root = line.split(" ")[0]
         lr = len(root)
+        tokens[root] = len(tokens)
         for i in range(lr):
             nroot = root[:i] + "N" + root[i+1:]
             nfirst = "#"*(k-lr) + nroot
@@ -41,56 +42,38 @@ with open("tokens.txt","w") as f:
         
 print(len(tokens))
 r = 0 
-with open(infile_good) as f:
+with open(infile) as f:
     for line in f:
         l = len(line.strip())
-        if l < 50:
-            continue
-        padded = "#"*n_ctx + line.strip() + "#" * n_ctx
-        for c in range(min([l,150])):
-            tkn = 1 # 1 = OOV
+        good, bad = line.strip().split("\t")
+        #if l < 50:
+        #    continue
+        padded_g = "#"*n_ctx + good + "#" * n_ctx
+        padded_b = "#"*n_ctx + bad + "#" * n_ctx
+#        for c in range(min([l,150])):
+        for c in range(len(good)):
+            tkn_g = 1 # 1 = OOV
             try:
-                tkn = tokens[padded[c:(c+2*n_ctx+1)]]
+                tkn_g = tokens[padded_g[c:(c+2*n_ctx+1)]]
             except:
                 pass
-            kmers_g[r][c] = tkn
-            labels_g[r] = 0
+            kmers[r][c] = tkn_g
+            labels[r] = 0
         r += 1
         if max_sz <= r:
             break
-kmers_g = kmers_g[:r]
-labels_g = labels_g[:r]
-kmers_b = np.zeros((max_sz,line_size),dtype='int32')
-labels_b = np.zeros(max_sz, dtype='int32')
-r = 0
-with open(infile_bad) as f:
-    for line in f:
-        l = len(line.strip())
-        if l < 50:
-            continue
-        padded = "#"*n_ctx + line.strip() + "#" * n_ctx
-        for c in range(min([l,150])):
-            tkn = 0
+        for c in range(len(bad)):
+            tkn_b = 1 # 1 = OOV
             try:
-                tkn = tokens[padded[c:(c+2*n_ctx+1)]]
+                tkn_b = tokens[padded_b[c:(c+2*n_ctx+1)]]
             except:
                 pass
-            kmers_b[r][c] = tkn
-            labels_b[r] = 1
+            labels[r] = 1
+            kmers[r][c] = tkn_b
         r += 1
         if max_sz <= r:
             break
-newlen = min([len(kmers_b),len(kmers_g)])
-kmers = np.zeros((newlen*2,line_size),dtype='int32')
-labels = np.zeros(newlen*2,dtype='int32')
-kmers[0::2] = kmers_g[:newlen]
-labels[0::2] = labels_g[:newlen]
-kmers[1::2] = kmers_b[:newlen]
-labels[1::2] = labels_b[:newlen]
-del kmers_b
-del kmers_g
-del labels_b
-del labels_g
+
 del tokens
 print(kmers.shape)
 data = tf.data.Dataset.from_tensor_slices((kmers, labels))
